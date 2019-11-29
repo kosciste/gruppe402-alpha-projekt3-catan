ec1c2e4740e850e5cb386a1f87b33474abeb3e85
Peter Blattmann <blattpet@students.zhaw.ch>
2019-11-29 23:13:53 +0100

    Klasse Player: Methoden für die Verfügbarkeit der Figuren ergänzen.

--- src/main/java/ch/zhaw/catan/Player.java
+++ src/main/java/ch/zhaw/catan/Player.java
@@ -13,17 +13,54 @@ public class Player {
 	private int winPoints;
 //	private static List<Faction> playerColors = new ArrayList<>();
 //	private static int colorIndex = 0;
-//	private List<Meeple> meeples;
+	private List<Meeple> meeples;
 	Faction playerFaction;
 	
 	
 	public Player(Config.Faction playerFaction) {
 		this.playerFaction = playerFaction;
-//		meeples = new ArrayList<>();
+		meeples = new ArrayList<>();
 		winPoints = 0;
 //		colorIndex++;
 	}
 	
+	
+	public boolean hasAvailableRoads() {
+		int usedRoads = 0;
+		Iterator<Meeple> it = meeples.iterator();
+		while(it.hasNext()) {
+			Meeple m1 = it.next();
+			if(m1 instanceof Road) {
+				usedRoads++;
+			}
+		}
+		return Config.Structure.ROAD.getStockPerPlayer() > usedRoads;
+	}
+	
+	 public boolean hasAvailableSettlements() {
+			int usedSettlements = 0;
+			Iterator<Meeple> it = meeples.iterator();
+			while(it.hasNext()) {
+				Meeple m1 = it.next();
+				if(m1 instanceof Settlement) {
+					usedSettlements++;
+				}
+			}
+			return Config.Structure.SETTLEMENT.getStockPerPlayer() > usedSettlements;
+	 }
+	 
+//	 public boolean hasAvailableCities() {
+//			int usedCities = 0;
+//			Iterator<Meeple> it = meeples.iterator();
+//			while(it.hasNext()) {
+//				Meeple m1 = it.next();
+//				if(m1 instanceof City) {
+//					usedCities++;
+//				}
+//			}
+//			return Config.Structure.CITY.getStockPerPlayer() > usedCities;
+//	 }
+	 
 	public int getWinPoints() {
 		return winPoints;
 	}
@@ -32,9 +69,18 @@ public class Player {
 		winPoints = newWinPoints;
 	}
 	
-	public void initializeMeeple() {
-//		meeples.add(new Meeple())
+	public void initializeRoad(Faction owner) {
+		meeples.add(new Road(owner));
 	}
+	
+	public void initializeSettlement(Faction owner) {
+		meeples.add(new Settlement(owner));
+	}
+	
+//	public void initializeCity(Faction owner) {
+//		meeples.add(new City(owner));
+//	}
+	
 	
 //TODO Liste in SidlerGame speichern??? Farbe wird dann an den Konstruktor
 	// von Player übergeben.
