fdd7b0b926249d51fdcfa90c8de40aa3838ea9e3
Peter Blattmann <blattpet@students.zhaw.ch>
2019-12-01 20:05:36 +0100

    Methoden und Kommentare ergänzen.

--- src/main/java/ch/zhaw/catan/Player.java
+++ src/main/java/ch/zhaw/catan/Player.java
@@ -16,7 +16,12 @@ public class Player {
 	private List<Meeple> meeples;
 	Faction playerFaction;
 	
-	
+	/**
+	 * The constructor creats a player with a list for his meeples.
+	 * The player is identified by his colour.
+	 * 
+	 * @param playerFaction The colour from faction.
+	 */
 	public Player(Config.Faction playerFaction) {
 		this.playerFaction = playerFaction;
 		meeples = new ArrayList<>();
@@ -24,7 +29,13 @@ public class Player {
 //		colorIndex++;
 	}
 	
-	
+	/**
+	 * This method counts the numbers of roads in the ArrayList of the 
+	 * player and checks the numbers with the maximal numbers of roads
+	 * who are available pro player.
+	 * 
+	 * @return true or false: for available roads.
+	 */
 	public boolean hasAvailableRoads() {
 		int usedRoads = 0;
 		Iterator<Meeple> it = meeples.iterator();
@@ -37,16 +48,24 @@ public class Player {
 		return Config.Structure.ROAD.getStockPerPlayer() > usedRoads;
 	}
 	
+	
+	/**
+	 * This method counts the numbers of settlements in the ArrayList of 
+	 * the player and checks the numbers with the maximal numbers of 
+	 * settlements who are available pro player.
+	 * 
+	 * @return true or false: for available settlements.
+	 */
 	 public boolean hasAvailableSettlements() {
-			int usedSettlements = 0;
-			Iterator<Meeple> it = meeples.iterator();
-			while(it.hasNext()) {
-				Meeple m1 = it.next();
-				if(m1 instanceof Settlement) {
-					usedSettlements++;
-				}
+		int usedSettlements = 0;
+		Iterator<Meeple> it = meeples.iterator();
+		while(it.hasNext()) {
+			Meeple m1 = it.next();
+			if(m1 instanceof Settlement) {
+				usedSettlements++;
 			}
-			return Config.Structure.SETTLEMENT.getStockPerPlayer() > usedSettlements;
+		}
+		return Config.Structure.SETTLEMENT.getStockPerPlayer() > usedSettlements;
 	 }
 	 
 //	 public boolean hasAvailableCities() {
@@ -60,22 +79,50 @@ public class Player {
 //			}
 //			return Config.Structure.CITY.getStockPerPlayer() > usedCities;
 //	 }
-	 
+
+	 /**
+	  * This method returns the colour of the player.
+	  * 
+	  * @return playerFaction
+	  */
+	public Faction getPlayerFaction() {
+		return playerFaction;
+	}
+	
+	/**
+	 * This method returns the victory points of the player. 
+	 * 
+	 * @return winPoints
+	 */
 	public int getWinPoints() {
 		return winPoints;
 	}
 	
+	/**
+	 * This Method set the victory points.
+	 * 
+	 * @param newWinPoints
+	 */
 	public void setWinPoints(int newWinPoints) {
 		winPoints = newWinPoints;
 	}
 	
-	public void initializeRoad(Faction owner) {
-		meeples.add(new Road(owner));
+	/**
+	 * This method saves one meeple in the ArrayList of the player.
+	 * 
+	 * @param meeple
+	 */
+	public void initializeMeeple(Meeple meeple) {
+		meeples.add(meeple);
 	}
 	
-	public void initializeSettlement(Faction owner) {
-		meeples.add(new Settlement(owner));
-	}
+//	public void initializeRoad(Faction owner) {
+//		meeples.add(new Road(owner));
+//	}
+	
+//	public void initializeSettlement(Faction owner) {
+//		meeples.add(new Settlement(owner));
+//	}
 	
 //	public void initializeCity(Faction owner) {
 //		meeples.add(new City(owner));
