5a631a66216ccc8fc4f22688b9d642021a5d2515
Peter Blattmann <blattpet@students.zhaw.ch>
2019-12-02 10:50:46 +0100

    Test Klasse erstellt.

---
+++ src/test/java/ch/zhaw/catan/PlayerTest.java
@@ -0,0 +1,84 @@
+package ch.zhaw.catan;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author blatt
+ *
+ */
+class PlayerTest {
+
+	@Test
+	public void startHasAvailableRoads() {
+		Player player = new Player(Config.Faction.RED);
+		assertTrue(player.hasAvailableRoads());
+	}
+
+	@Test
+	public void normalHasAvailableRoads() {
+		Player player = new Player(Config.Faction.RED);
+		player.initializeMeeple(new Road(Config.Faction.RED));
+		player.initializeMeeple(new Road(Config.Faction.RED));
+		player.initializeMeeple(new Road(Config.Faction.RED));
+		assertTrue(player.hasAvailableRoads());
+	}
+	
+	@Test
+	public void maxHasAvailableRoads() {
+		Player player = new Player(Config.Faction.RED);
+		player.initializeMeeple(new Road(Config.Faction.RED));
+		player.initializeMeeple(new Road(Config.Faction.RED));
+		player.initializeMeeple(new Road(Config.Faction.RED));
+		player.initializeMeeple(new Road(Config.Faction.RED));
+		player.initializeMeeple(new Road(Config.Faction.RED));
+		player.initializeMeeple(new Road(Config.Faction.RED));
+		player.initializeMeeple(new Road(Config.Faction.RED));
+		player.initializeMeeple(new Road(Config.Faction.RED));
+		player.initializeMeeple(new Road(Config.Faction.RED));
+		player.initializeMeeple(new Road(Config.Faction.RED));
+		player.initializeMeeple(new Road(Config.Faction.RED));
+		player.initializeMeeple(new Road(Config.Faction.RED));
+		player.initializeMeeple(new Road(Config.Faction.RED));
+		player.initializeMeeple(new Road(Config.Faction.RED));
+		player.initializeMeeple(new Road(Config.Faction.RED));
+		assertFalse(player.hasAvailableRoads());
+	}
+	
+	@Test
+	public void startHasAvailableSettlements() {
+		Player player = new Player(Config.Faction.RED);
+		assertTrue(player.hasAvailableSettlements());
+	}
+	
+	@Test
+	public void normalHasAvailableSettlements() {
+		Player player = new Player(Config.Faction.RED);
+		player.initializeMeeple(new Settlement(Config.Faction.RED));
+		player.initializeMeeple(new Settlement(Config.Faction.RED));
+		player.initializeMeeple(new Settlement(Config.Faction.RED));
+		assertTrue(player.hasAvailableSettlements());
+	}
+	
+	@Test
+	public void maxHasAvailableSettlements() {
+		Player player = new Player(Config.Faction.RED);
+		player.initializeMeeple(new Settlement(Config.Faction.RED));
+		player.initializeMeeple(new Settlement(Config.Faction.RED));
+		player.initializeMeeple(new Settlement(Config.Faction.RED));
+		player.initializeMeeple(new Settlement(Config.Faction.RED));
+		player.initializeMeeple(new Settlement(Config.Faction.RED));
+		player.initializeMeeple(new Settlement(Config.Faction.RED));
+		player.initializeMeeple(new Settlement(Config.Faction.RED));
+		player.initializeMeeple(new Settlement(Config.Faction.RED));
+		player.initializeMeeple(new Settlement(Config.Faction.RED));
+		player.initializeMeeple(new Settlement(Config.Faction.RED));
+		player.initializeMeeple(new Settlement(Config.Faction.RED));
+		player.initializeMeeple(new Settlement(Config.Faction.RED));
+		player.initializeMeeple(new Settlement(Config.Faction.RED));
+		player.initializeMeeple(new Settlement(Config.Faction.RED));
+		player.initializeMeeple(new Settlement(Config.Faction.RED));
+		assertFalse(player.hasAvailableSettlements());
+	}
+}
