424df7307cd4628556eadb05ac77a3f8408da648
Stefan Koscica <kosciste@students.zhaw.ch>
2019-12-03 03:06:40 +0100

    Abffagen verbessert

--- src/main/java/ch/zhaw/catan/SiedlerGame.java
+++ src/main/java/ch/zhaw/catan/SiedlerGame.java
@@ -6,179 +6,239 @@ import ch.zhaw.catan.Config.Resource;
 import java.awt.Point;
 import java.util.*;
 
+/**
+ * author: kosciste
+ */
+
 public class SiedlerGame {
 
-  SiedlerBoard board = new SiedlerBoard();
-  private int winPoints;
-  private int numberOfPlayers;
-  private List<Player> players = new ArrayList<>();
+    private static int playerAtTurn = 0;
+    SiedlerBoard board = new SiedlerBoard();
+    private int winPoints;
+    private int numberOfPlayers;
+    private List<Player> players = new ArrayList<>();
 
-  private static int playerAtTurn = 0;
+    public SiedlerGame(int winPoints, int numberOfPlayers) {
+        this.winPoints = winPoints;
+        this.numberOfPlayers = numberOfPlayers;
+        for (int i = 0; i < numberOfPlayers; i++) {
 
-  public SiedlerGame(int winPoints, int numberOfPlayers) {
-    this.winPoints = winPoints;
-    this.numberOfPlayers = numberOfPlayers;
-    for(int i = 0; i < numberOfPlayers; i++){
-
-     Player player =  new Player(Faction.values()[i]);
-     players.add(player);
-    }
-  }
-
-  /**
-   * This method switches to next player who needs to make a turn.
-   */
-  public void switchToNextPlayer() {
-    if(playerAtTurn<4) {
-
-      playerAtTurn++;
-    }
-
-    playerAtTurn = 0;
-  }
-
-  /**
-   * This method switches to the previous player who needs to make a turn.
-   */
-  public void switchToPreviousPlayer() {
-    if(playerAtTurn>0) {
-      playerAtTurn--;
+            Player player = new Player(Faction.values()[i]);
+            players.add(player);
+        }
+        placeInitialSettlement(new Point(4,0));
 
     }
 
-    playerAtTurn = 4;
-  }
+    /**
+     * This method switches to next player who needs to make a turn.
+     */
+    public void switchToNextPlayer() {
+        if (playerAtTurn < 4) {
+
+            playerAtTurn++;
+        }
+
+        playerAtTurn = 0;
+    }
+
+    /**
+     * This method switches to the previous player who needs to make a turn.
+     */
+    public void switchToPreviousPlayer() {
+        if (playerAtTurn > 0) {
+            playerAtTurn--;
+
+        }
+
+        playerAtTurn = 4;
+    }
 
 
-  /**
-   * This method returns a list with all players.
-   * @return List with all players
-   */
-  public List<Player> getPlayer() {
+    /**
+     * This method returns a list with all players.
+     *
+     * @return List with all players
+     */
+    public List<Player> getPlayer() {
 
-    return players;
-  }
-
-
-  /**
-   * This method returns the GameBoard
-   * @return the GameBoard
-   */
-  public SiedlerBoard getBoard() {
-
-    return board;
-  }
+        return players;
+    }
 
 
-  /**
-   * Returns the current player who needs to make a turn.
-   * @return the current player
-   */
-  public Player getCurrentPlayer() {
-   return players.get(playerAtTurn);
-  }
+    /**
+     * This method returns the GameBoard
+     *
+     * @return the GameBoard
+     */
+    public SiedlerBoard getBoard() {
+
+        return board;
+    }
 
 
-
-  public int getCurrentPlayerResourceStock(Resource resource) {
-    // TODO: Implement
-    return 0;
-  }
-
-
-  /**
-   * Builds an  initial settlement
-   * @param position the specified position for the settlement
-   * @return true, if the settlement has been built
-   */
-  public boolean placeInitialSettlement(Point position) {
-
-  return buildSettlement(position);
-
-  }
-
-  /**
-   * Builds an initial road
-   * @param roadStart the Point where the road starts
-   * @param roadEnd the Point where the road ends
-   * @return true, if the road has been built
-   */
-  public boolean placeInitialRoad(Point roadStart, Point roadEnd) {
-
-    return buildRoad(roadStart,roadEnd);
-
-  }
+    /**
+     * Returns the current player who needs to make a turn.
+     *
+     * @return the current player
+     */
+    public Player getCurrentPlayer() {
+        return players.get(playerAtTurn);
+    }
 
 
-  public Map<Faction, List<Resource>> throwDice(int dicethrow) {
-    // TODO: Implement
-    return null;
-  }
+    public int getCurrentPlayerResourceStock(Resource resource) {
+        // TODO: Implement
+        return 0;
+    }
 
 
-  /**
-   * This method builds a settlement at a specified position
-   * @param position the specified position for the settlement
-   * @return
-   */
-  public boolean buildSettlement(Point position) {
+    /**
+     * Builds an  initial settlement
+     *
+     * @param position the specified position for the settlement
+     * @return true, if the settlement has been built
+     */
+    public boolean placeInitialSettlement(Point position) {
 
-    //TODO: Überprüfung ob die Strasse des Player an Corner grenzt
+      Settlement settlement = new Settlement(getCurrentPlayer().getPlayerFaction());
 
-    Settlement settlement = new Settlement(getCurrentPlayer().getPlayerFaction());
-
-      if(board.hasCorner((position))&&board.getCorner(position) == null && board.getAdjacentEdges(position)!=null
-              && getCurrentPlayer().hasAvailableSettlements()) {
+      if (board.hasCorner(position)
+              && board.getCorner(position) == null
+              && getCurrentPlayer().hasAvailableSettlements()
+              && board.getNeighboursOfCorner(position).isEmpty()) {
 
         getCurrentPlayer().initializeMeeple(settlement);
 
-        board.setCorner(position,settlement.toString());
+        board.setCorner(position, settlement.toString());
         return true;
+      }
+      else {
+        return false;
       }
 
 
-    else {
-      return false;
     }
 
-  }
+    /**
+     * Builds an initial road
+     *
+     * @param roadStart the Point where the road starts
+     * @param roadEnd   the Point where the road ends
+     * @return true, if the road has been built
+     */
+    public boolean placeInitialRoad(Point roadStart, Point roadEnd) {
 
 
-  public boolean buildCity(Point position) {
-    // TODO: OPTIONAL task - Implement
-    return false;
-  }
+        return buildRoad(roadStart, roadEnd);
 
-  /**
-   * This method builds a road at a specicfied location
-   * @param roadStart the Point where the road starts
-   * @param roadEnd the Point where the road ends
-   * @return true, if the road has been built
-   */
-  public boolean buildRoad(Point roadStart, Point roadEnd) {
-
-    Road road = new Road(getCurrentPlayer().getPlayerFaction());
-
-  if(board.hasEdge(roadStart,roadEnd)&&board.getEdge(roadStart, roadEnd)==null)  {
-
-    board.setEdge(roadStart, roadEnd, road.toString());
-    return true;
-  }
-
-  else {
-    return false;
-  }
+    }
 
 
-  }
+    public Map<Faction, List<Resource>> throwDice(int dicethrow) {
+        // TODO: Implement
+        return null;
+    }
 
-  public boolean tradeWithBankFourToOne(Resource offer, Resource want) {
-    // TODO: Implement
-    return false;
-  }
 
-  public Faction getWinner() {
-    // TODO: Implement
-    return null;
-  }
+    /**
+     * This method builds a settlement at a specified position
+     *
+     * @param position the specified position for the settlement
+     * @return
+     */
+    public boolean buildSettlement(Point position) {
+      //TODO: Überprüfen und abziehen der Rohstoffe
+        Settlement settlement = new Settlement(getCurrentPlayer().getPlayerFaction());
+        boolean hasAdjacentRoads = false;
+        List<Meeple> meeples = getCurrentPlayer().getMeepleList();
+        for (int i = 0; i < meeples.size(); i++) {
+
+            if (board.getAdjacentEdges(position).contains(meeples.get(i).toString())) {
+                hasAdjacentRoads = true;
+            }
+        }
+        if (board.getCorner(position) == null && hasAdjacentRoads
+                && getCurrentPlayer().hasAvailableSettlements() && board.getNeighboursOfCorner(position).isEmpty()) {
+
+            getCurrentPlayer().initializeMeeple(settlement);
+
+            board.setCorner(position, settlement.toString());
+            return true;
+        }
+          else {
+            return false;
+        }
+    }
+
+
+    public boolean buildCity(Point position) {
+        // TODO: OPTIONAL task - Implement
+        return false;
+    }
+
+    /**
+     * This method builds a road at a specicfied location
+     *
+     * @param roadStart the Point where the road starts
+     * @param roadEnd   the Point where the road ends
+     * @return true, if the road has been built
+     */
+    public boolean buildRoad(Point roadStart, Point roadEnd) {
+        Road road = new Road(getCurrentPlayer().getPlayerFaction());
+        boolean hasAdjacentElements = false;
+        List<String> meeples = new ArrayList<>();
+        for(Meeple meeple : getCurrentPlayer().getMeepleList()){
+          meeples.add(meeple.toString());
+        }
+
+        for(int i = 0; i < meeples.size() ;i++){
+
+                if(board.getAdjacentEdges(roadStart).contains(meeples.get(i))|| board.getCorner(roadStart).equals(meeples.get(i))) {
+
+                    if(board.getCorner(roadEnd) == null ||meeples.contains(board.getCorner(roadEnd))){
+                        hasAdjacentElements = true;
+                    }
+                }
+
+
+/**
+          if((!(board.getAdjacentEdges(roadStart).isEmpty())
+                  && board.getAdjacentEdges(roadEnd).
+                  contains(meeples.get(i)))
+                  || board.getCorner(roadEnd).
+                  equals(meeples.get(i))) {
+
+            if(board.getCorner(roadStart) == null ||meeples.contains(board.getCorner(roadStart))){
+              hasAdjacentElements = true;
+            }
+          }
+ */
+
+
+        }
+
+        if (board.hasEdge(roadStart, roadEnd) && board.getEdge(roadStart, roadEnd) == null && hasAdjacentElements) {
+
+            getCurrentPlayer().initializeMeeple(road);
+            board.setEdge(roadStart, roadEnd, road.toString());
+
+            return true;
+        }
+        else {
+            return false;
+        }
+
+
+    }
+
+    public boolean tradeWithBankFourToOne(Resource offer, Resource want) {
+        // TODO: Implement
+        return false;
+    }
+
+    public Faction getWinner() {
+        // TODO: Implement
+        return null;
+    }
 }
