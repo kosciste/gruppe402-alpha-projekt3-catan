d3db7211121334c0368b2a8a8355276f93a3a36f
Peter Blattmann <blattpet@students.zhaw.ch>
2019-12-04 23:47:26 +0100

    Klasse City erstellt.

---
+++ src/main/java/ch/zhaw/catan/City.java
@@ -0,0 +1,27 @@
+package ch.zhaw.catan;
+
+/**
+ * This class models a city. A city is the next level from a settlement.
+ * The city gives two point of resources back and also two winpoints.
+ * 
+ * @author blatt
+ *
+ */
+public class City extends Meeple {
+
+	
+	
+	/**
+	 * Creats a new city with a specified owner.
+	 * 
+	 * @param owner The owner of the city
+	 */
+	public City(Config.Faction owner) {
+		super(owner);
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+}
