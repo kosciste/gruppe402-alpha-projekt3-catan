8606c77622ee868aca89c42407e186fe3e9fe79c
Stefan Koscica <kosciste@students.zhaw.ch>
2019-12-05 11:24:25 +0100

    Testklasse SiedlerGame implementiert

---
+++ src/test/java/ch/zhaw/catan/SiedlerGameTest.java
@@ -0,0 +1,65 @@
+package ch.zhaw.catan;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SiedlerGameTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void switchToNextPlayer() {
+    }
+
+    @Test
+    void switchToPreviousPlayer() {
+    }
+
+    @Test
+    void getPlayer() {
+    }
+
+    @Test
+    void getBoard() {
+    }
+
+    @Test
+    void getCurrentPlayer() {
+    }
+
+    @Test
+    void placeInitialSettlement() {
+    }
+
+    @Test
+    void placeInitialRoad() {
+    }
+
+    @Test
+    void throwDice() {
+    }
+
+    @Test
+    void buildSettlement() {
+    }
+
+    @Test
+    void buildRoad() {
+    }
+
+    @Test
+    void buildCity() {
+    }
+
+    @Test
+    void tradeWithBankFourToOne() {
+    }
+
+    @Test
+    void getWinner() {
+    }
+}
